93953bdb4965fb89017e959fc5dfa9ddb638d362
emrullahardc <emrullahardc@gmail.com>
2023-09-28 09:42:45 +0300

    redirect trait added.

---
+++ src/Traits/RedirectBackTrait.php
@@ -0,0 +1,41 @@
+<?php namespace Visiosoft\GlobalHelperExtension\Traits;
+
+use Illuminate\Http\RedirectResponse;
+
+trait RedirectBackTrait
+{
+    /**
+     * @param bool $withInput
+     * @return RedirectResponse
+     */
+    public function redirectBack(bool $withInput = true): RedirectResponse
+    {
+        $redirect = $this->redirect->back();
+        if ($withInput) {
+            $redirect->withInput(request()->all());
+        }
+
+        return $redirect;
+    }
+
+    /**
+     * @param $message
+     * @param bool $withInput
+     * @return RedirectResponse
+     */
+    public function redirectBackWithSuccess($message, bool $withInput = false): RedirectResponse
+    {
+        return $this->redirectBack($withInput)->with('success', ['message' => $message]);
+    }
+
+    /**
+     * @param $message
+     * @param bool $withInput
+     * @return RedirectResponse
+     */
+    public function redirectBackWithError($message, bool $withInput = true): RedirectResponse
+    {
+        return $this->redirectBack($withInput)->with('error', ['message' => $message]);
+    }
+
+}
